55f98cb167b492033a0d12b41586a36db022e589
juliastenstrom <sj7988381@gmail.com>
2026-03-23 15:14:36 +0100

    hantera fil
    Läser in en fil och skickar tillbaka den med lowercase samt splitad data i ord för for

--- frontend/src/pages/keyword.js
+++ frontend/src/pages/keyword.js
@@ -1,11 +1,29 @@
 //The test to find if a keyword could be recognised 
 
+const path = require('path');
 const fs = require('fs');
 
-fs.readFile('test.txt','utf8', (err,data)=> {
+const inputPath = path.join(__dirname, 'test.txt');
+const outputPath = path.join(__dirname, 'output.txt');
+
+
+fs.readFile(inputPath, 'utf8', (err, data) => {
     if(err){
         console.error(err);
         return;
     }
-    console.log(data);
-})
+    const keyword = "han"
+    const lowerText = data.toLowerCase();
+
+    //alla ord för sig
+    const split = lowerText.split(/[\r\n. ]+/);
+    console.log(split)
+    fs.writeFile(outputPath, lowerText, (err) =>{
+        if(err){
+            console.error(err)
+            return;
+        }
+        console.log("lowercase!")
+    })
+
+});
